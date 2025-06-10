cd499c17748da7da7c2416a54f7aa3e181a71366
nadiadweik <nadiadweik02@gmail.com>
2025-06-09 23:27:35 -0500

    Update Login.js

--- ui/src/Login.js
+++ ui/src/Login.js
@@ -51,8 +51,8 @@ export default function Login({ onLogin }) {
           onChange={handleChange}
           required
         />
-        <Typography fontSize={16}>
-          Email and Password are Case Sensetive
+        <Typography fontSize={16} sx={{ paddingBottom: 2 }}>
+          ⚠️ Email and Password are Case Sensetive ⚠️
           </Typography>
         <Button fullWidth variant="contained" type="submit">Log In</Button>
       </Box>
